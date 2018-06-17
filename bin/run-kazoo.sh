05680e6ad9855628a194648550846fb2bf8ecac1
agplacid <b.ikwen@ag-placid.com>
2018-06-18 00:08:13 +0100

    Update run-kazoo.sh

--- bin/run-kazoo.sh
+++ bin/run-kazoo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FLAGS=${1:-"-td"}
 NETWORK=${NETWORK:-"kazoo"}
-KAZOO_APPS=${APPS:-"sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr"}
+KAZOO_APPS=${APPS:-"acdc,ananke,call_inspector,camper,cccp,doodle,edr,frontier,hotornot,jogny5,konami,notify,pusher,skel,stats,tasks,sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,spyvsspy,teletype,trunkstore,webhooks,ecallmgr"}
 NAME=kazoo.$NETWORK
 if [ -n "$(docker ps -aq -f name=$NAME)" ]
 then
